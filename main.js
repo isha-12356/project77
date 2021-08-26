b5823be6af4360c49b7670b83672dc2249474c0a
isha-12356 <85653561+isha-12356@users.noreply.github.com>
2021-08-25 19:00:02 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("my first console project");
+
+var first_name = "Isha";
+
+var last_name = "Mishra";
+
+var fullname = first_name.concat( last_name);
+
+console.log(fullname);
+
+var a = 10;
+
+var b = 40;
+
+console.log(b);
+
+console.log(a);
+
+var c= a + b;
+
+console.log(c);
+
